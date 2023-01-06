d72ef06469b7c252c0fb3b1fcda745c1af8f50f9
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-01-06 08:56:48 +0800

    add route shortUrl get and post

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const url = require('url');
+const bodyParser = require('body-parser')
+
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +23,52 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+app.post("/api/shorturl", (req, res) => {
+  const arrLink = []
+  const link = req.body.url
+  // console.log(req.body)
+  const parsedUrl = url.parse(link);
+  const hostname = parsedUrl.hostname;
+  dns.lookup(hostname, (err, address, family) => {
+    if (err) {
+      console.error(`DNS lookup failed: ${err.message}`);
+      return;
+    }
+  
+    console.log(`IP address: ${address}`);
+    console.log(`IP family: IPv${family}`);
+  });
+
+  const randomNumber = Math.floor(Math.random() * 10000);
+
+  const object = {[randomNumber]: link}
+  arrLink.push(object)
+  console.log(arrLink)
+
+  res.json({original_url: link, short_url: randomNumber})
+})
+
+
+app.get("/api/shorturl/:link", (req, res) => {
+  const link = req.params.link
+  // console.log(arrLink)
+
+  // const found = arrLink.some(function(object) {
+  //   return link in object;
+  // });
+  // console.log(found)
+  
+  const value = arrLink.find(function(object) {
+    return link in object;
+  });
+
+  // console.log(value[link])
+
+  res.redirect(value[link]);
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
